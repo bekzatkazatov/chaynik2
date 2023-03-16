959d74c373d0025485f805a4af7840db11c2daba
bekzatkazatov <bekzatx2x@gmail.com>
2023-03-16 16:20:19 +0600

    chay2

---
+++ script.js
@@ -0,0 +1,43 @@
+//  const num = [1, 2, 3, 0, 4, 5, 6]
+// const num2 = num.map (el => el * el )
+//  console.log(num2)
+
+// const num = [1, 2, 3, 0, 4, 5, 6]
+// const num2 = num.map (function (el){
+//     return el > 0
+// })
+// console.log(num2)
+
+// const num = [1,2,-3,0,-5,6]
+// const num2 = num.filter(el => el >= 0 )
+//      if (num2.length === num.length){
+//           console.log(true)
+//      }else {
+//           console.log(false)
+//      }
+
+
+
+// const num = [1,2,-3,0,4,5,-6]
+// const num2 = num.filter(el => el < 0)
+// console.log(num2)
+
+// const num = [1, -2, 3, 0, 4, -5, 6]
+// console.log (num.filter(el => el % 2 === 0))
+
+ // const str = ['hello', 'sit', 'ipsum', 'javascript', 'python', 'sun', 'even']
+ //  const str2 = (str.filter(el => el.length > 5))
+ //  console.log (str2)
+
+// const num = [1, 2, -3, 0, -4, 5, -6]
+// const num2 = (num.filter(el => el < 0 ))
+// console.log (num2.length)
+
+// const num = [1, 2, 3, 33, 4, 5, 6]
+// const num2 = num.reduce((acc  , el )=>{
+//     return  acc + el
+// })
+// console.log(num2)
+
+
+
